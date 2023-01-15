f9f0e87f3a5b12b6e8adfe2439580a912102b62f
bobstanleygardner <94706916+bobstanleygardner@users.noreply.github.com>
2023-01-16 00:32:15 +0530

    Add files via upload

---
+++ data/StoryOfMatthew_2.js
@@ -0,0 +1,13 @@
+var json_StoryOfMatthew_2 = {
+"type": "FeatureCollection",
+"name": "StoryOfMatthew_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "X": 35.2, "Y": 31.7, "1": "1. Church of Nativity" }, "geometry": { "type": "Point", "coordinates": [ 35.2, 31.7 ] } },
+{ "type": "Feature", "properties": { "X": 35.55, "Y": 31.83, "1": "2. Bethany Beyond the Jordan" }, "geometry": { "type": "Point", "coordinates": [ 35.55, 31.83 ] } },
+{ "type": "Feature", "properties": { "X": 35.55, "Y": 32.88, "1": "3. Mount of Beatitudes" }, "geometry": { "type": "Point", "coordinates": [ 35.55, 32.88 ] } },
+{ "type": "Feature", "properties": { "X": 35.39, "Y": 32.68, "1": "4. Mount Tabor" }, "geometry": { "type": "Point", "coordinates": [ 35.39, 32.68 ] } },
+{ "type": "Feature", "properties": { "X": 35.5, "Y": 32.91, "1": "5. Gennesaret" }, "geometry": { "type": "Point", "coordinates": [ 35.5, 32.91 ] } },
+{ "type": "Feature", "properties": { "X": 35.23, "Y": 31.77, "1": "6. Golgata" }, "geometry": { "type": "Point", "coordinates": [ 35.23, 31.77 ] } }
+]
+}
